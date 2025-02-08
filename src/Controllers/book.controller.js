728bf741bf3951f4669389220e20516aa3fec423
K Bala Dattu <kbaladattu@gmail.com>
2025-02-08 17:51:49 +0530

    Added crud funtionalities for the books with file upload and delete in the imagekit.io

--- src/Controllers/book.controller.js
+++ src/Controllers/book.controller.js
@@ -1,45 +1,24 @@
-import imagekitSetup from "../Config/imagekit.js";
 import Book from "../Models/books.model.js"
+import { deleteFromImagekit, uploadToImagekit } from "../Utils/imagekit-service.js";
 
 let dummyImage = "https://img.freepik.com/free-vector/blank-book-cover-white-vector-illustration_1284-41903.jpg?t=st=1738850444~exp=1738854044~hmac=14fe4ab743d8e3961edd500b97daff73b1f90f1613969cfc358b80277f385a75&w=740";
 
-// Function to upload an image or PDF to ImageKit
-const uploadFile = async (fileData = "e:\WRsLEAAAQBAJ.jpg", fileName = "title") => {
-    try {
-        console.log(fileData)
-        const result = await imagekitSetup.upload({
-            file: fileData, // base64-encoded file or a URL
-            fileName: fileName, // any file name you want
-        });
-        console.log(result);
-        return result;
-    } catch (error) {
-        throw new Error('Image/PDF upload failed');
-    }
-};
-
-// Function to delete a file from ImageKit
-const deleteFile = async (fileId) => {
-    try {
-        await imagekitSetup.deleteFile(fileId);
-    } catch (error) {
-        throw new Error('Failed to delete file from ImageKit');
-    }
-};
 
 export const addBook = async (req, res, next) => {
     try {
-        const { title, author, description,coverImage, pdfUrl, genre } = req.body;
-        console.log(req.body);
-        const fileUrl = dummyImage;
-        if(coverImage)
-            fileUrl = await uploadFile();
+        const { title, author, description, pdfUrl, genre } = req.body;
+        let fileUrl = null;
+        console.log(req.file);
+        if (req.file)
+            fileUrl = await uploadToImagekit(req.file);
+
+        console.log(fileUrl);
         const newBook = new Book({
             title,
             author,
             description,
             pdfUrl,
-            coverImage: fileUrl ? fileUrl : dummyImage,
+            coverImage: fileUrl ? fileUrl.url : dummyImage,
             genre
         })
         await newBook.save();
@@ -71,16 +50,25 @@ export const updateBook = async (req, res, next) => {
     try {
         const { id } = req.params;
         const { name, genre, description } = req.body;
+        let updateCoverImage = null;
 
         const book = await Book.findById(id);
         if (!book) return res.status(404).json({ error: 'Book not found' });
 
+        let newCoverImage = book.coverImage;
+        let oldCoverImage = { url: book.coverImage, fileId: book.fileId };
+
+        if (req.file) {
+            newCoverImage = await uploadToImagekit(req.file, 'coverImage');
+        }
+
         book.name = name || book.name;
         book.genre = genre || book.genre;
+        book.coverImage = newCoverImage.url;
         book.description = description || book.description;
 
         await book.save();
-        res.status(200).json(book);
+        res.status(200).json({ message: "Book updated successfully", book });
     } catch (error) {
         res.status(500).json({ error: 'Failed to update book' });
         next(err);
@@ -94,6 +82,9 @@ export const deleteBookById = async (req, res, next) => {
 
         if (!book) return res.status(404).json({ error: 'Book not found' });
 
+        let coverImage ={url: book.coverImage, fileId: book.fileId};
+        await deleteFromImagekit(coverImage);
+        
         await Book.findByIdAndDelete(id);
 
         res.status(200).json({ message: 'Book deleted successfully' });
--- src/Routes/book.routes.js
+++ src/Routes/book.routes.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import upload from '../Middlewares/upload.middleware.js'
 import { addBook, searchBooks, updateBook, deleteBookById } from '../Controllers/book.controller.js'
+import upload from '../Middlewares/upload.middleware.js';
 
 const router = express.Router()
 
-router.post('/add', addBook);
+router.post('/add', upload.single('coverImage'), addBook);
 router.get('/search', searchBooks);
-router.put('/update/:id', updateBook);
+router.put('/update/:id', upload.single('coverImage'), updateBook);
 router.delete('/delete/:id', deleteBookById);
 
 export default router
