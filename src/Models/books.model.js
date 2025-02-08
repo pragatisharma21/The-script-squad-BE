9ac0ddd6f847fc1f470b77532a168ae2c39baf5c
pragatisharma21 <pragatis212@gmail.com>
2025-02-08 16:59:53 +0530

    updated book model

--- src/Models/books.model.js
+++ src/Models/books.model.js
@@ -4,13 +4,18 @@ const bookSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
     author: { type: String, required: true },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
     description: { type: String, required: true },
     pdfUrl: { type: String, required: true },
     pdfFileId: { type: String },
     coverImage: { type: String, required: true },
     coverImageFileId: { type: String },
     genre: { type: String, required: true },
-    price: { type: Number, required: true, min: 1},
+    price: { type: Number, required: true, min: 1 },
     reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }],
     comments: [
       {
